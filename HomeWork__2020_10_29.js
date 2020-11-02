d868244dc1c870fa5329ed3480ddb0eba81b81ce
Serhiy <46563664+SerhiyAndryeyev@users.noreply.github.com>
2020-11-02 15:48:53 +0200

    new 29.10

--- HomeWork__2020_10_29.js
+++ HomeWork__2020_10_29.js
@@ -1,121 +1,7 @@
 // ДЗ
 
-// }/ let str = 'dassdasd';
-// let n = -6;
-// let b = true;
-//
-// let und = undefined;
-// let wife = null;
-//
-// let obj = {};
-// let symb = Symbol('s');
-//
-// let bN = BigInt(982314804824283);
-//
-//
-//
-// const me = {
-//   name: 'Victor',
-//   age: 24,
-//   password: '12345',
-//   job: {
-//     company: {
-//       name: 'Inoxoft',
-//       street: 'H.Upa 73'
-//     },
-//     title: 'Middle+ Dev'
-//   },
-//   phone: 'Samsung'
-// }
-//
-// // console.log(me);
-// // console.log(me.job.company.name);
-//
-// me.car = 'Lacetti';
-// me.bike = 'Fazer';
-// me.age = 25;
-//
-// me.job.payment = 99999999;
-//
-// delete me.job.title;
-// delete me.password
-//
-// console.log(me);
-//
-//
-// const kari = {
-//   name: 'karina',
-//   age: 16
-// }
-//
-//
-// // const group = ['Alexandr', 'Dima', 'Anrii', 'Anna'];
-// // const arrMe = ['Vcitro', 24, false, true, 'Fazer', 'Inoxoft', 5474575465403, 6767];
-//
-// const deletedusers = [];
-//
-// let users = [me, kari, {model: 3}, true, 'Tesla', [2, 5, ['Hello', 'World']]];
-//
-// // console.log(users);
-// // console.log(users[1].age);
-// // console.log(users[5][2][0]);
-//
-// // group[4] = 'Max';
-// // console.log(group[666]);
-//
-// group.push('Anton');
-// group.unshift('Oleg');
-//
-// let deletedUser = group.pop();
-// alert(deletedUser + ' is deleted')
-// deletedusers.push(deletedUser)
-//
-// group.pop()
-// deletedUser = group.shift();
-// deletedusers.push(deletedUser)
-//
-// deletedUser = group.shift();
-// deletedusers.push(deletedUser)
-//
-// alert(deletedUser + ' is deleted')
-//
-// console.log(group);
-// console.log(deletedusers);
-// // console.log(ant);
-// // console.log(oleg);
-
-
-// const group = ['Alexandr', 'Dima','Anrii', 'Anna', 'Oleksii',];
-
-// console.log(group[0])
-// console.log(group[1])  => BAD
-// console.log(group[2])
-// console.log(group[3])
-
-// console.log('*************');
-// console.log(group.length);
-// console.log(group);
-// console.log('*************');
-
-
-// i = i + 1
-// i += 1
-// i++
-
-// for (let i = 0; i < group.length; i++) {
-//   console.log(group[i])
-//   console.log(i);
-//   console.log('__________________');
-// }
-
-// let i = 0;
-
-// while (i < group.length) {
-//   console.log(group[i]);
-//   i+=2
-// }
-
 // Практика 29.10.2020
+
 // - Дан масив ['a', 'b', 'c']. Додайте йому в кінець елементи 1, 2, 3 за допомогою циклу.
 
 // let array_1 = ['a', 'b', 'c'];
@@ -205,7 +91,6 @@
 let arr = [1, 2, 3, 4, 5];
 arr = arr.slice(0, 2);
 document.write(arr);
-
 
 //- Дан масив [1, 2, 3, 4, 5]. За допомогою методу/функції splice зробіть з нього масив [1, 'a', 'b', 2, 3, 4, 'c', 5, 'e'].
 //- Взяти масив з 10 чисел або створити його. Вивести в консоль тільки ті елементи, значення яких є парними.
